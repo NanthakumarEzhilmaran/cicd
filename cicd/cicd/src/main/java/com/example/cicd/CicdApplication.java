5ba1809d243589bf87ffa7592b6180b4b6fd5286
Nanthakumar <nanthakumar.ezhilmaran@soprasteria.com>
2025-02-25 16:01:21 +0530

    test

---
+++ cicd/cicd/src/main/java/com/example/cicd/CicdApplication.java
@@ -0,0 +1,16 @@
+package com.example.cicd;
+
+import ch.qos.logback.core.net.SyslogOutputStream;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CicdApplication.class, args);
+
+		System.out.println("Hi main");
+	}
+
+}
---
+++ cicd/cicd/src/main/java/com/example/cicd/HomeController.java
@@ -0,0 +1,16 @@
+package com.example.cicd;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController
+{
+
+
+    @GetMapping("/hello")
+            public String welcome(){
+        return "Hi";
+        }
+}
